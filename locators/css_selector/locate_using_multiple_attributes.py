30f4c77dae631e3c21091c77024ca922a3fedfe9
Raed Eleyan <raedeleyan1@gmail.com>
2025-03-23 00:41:03 +0200

    Implemented a script that locates a WebElement using the Multiple Attributes CSS Selector strategy

---
+++ locators/css_selector/locate_using_multiple_attributes.py
@@ -0,0 +1,17 @@
+"""
+This script locates a WebElement using the Multiple Attributes CSS Selector strategy.
+
+@author: Raed Eleyan.
+@date: 03/22/2025.
+@contact: raedeleyan1@gmail.com
+"""
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get('https://the-internet.herokuapp.com/login')
+driver.maximize_window()
+login_button = driver.find_element(By.CSS_SELECTOR, 'button[type="submit"][class="radius"]')
+driver.quit()
