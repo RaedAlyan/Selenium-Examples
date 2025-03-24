fa15bfd3021c0618b0080afa2d6eb7c8fd027063
Raed Eleyan <raedeleyan1@gmail.com>
2025-03-24 03:15:03 +0200

    Implemented a script that locates the dynamic dropdown WebElement

---
+++ advanced_web_elements/dropdown/locate_dynamic_dropdown.py
@@ -0,0 +1,24 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get('https://rahulshettyacademy.com/dropdownsPractise/')
+driver.maximize_window()
+# locate the dropdown WebElement.
+dropdown = driver.find_element(By.ID, 'autosuggest')
+# Send some keys to the dropdown WebElement.
+dropdown.send_keys('ind')
+# Wait for options to be loaded.
+time.sleep(2)
+# locate the link option that contains Australia as its visible text.
+desired_options = driver.find_elements(By.XPATH, '//li[@class="ui-menu-item"]/child::a')
+# Select the desired option
+for option in desired_options:
+    if option.text == 'India':
+        option.click()
+        break
+driver.quit()
