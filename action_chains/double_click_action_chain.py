b8cdc6902379d71bff0cc44f72ab2f141492b748
Raed Eleyan <raedeleyan1@gmail.com>
2025-04-03 08:13:05 +0300

    Updated some scripts by adding file header docstring

--- action_chains/double_click_action_chain.py
+++ action_chains/double_click_action_chain.py
@@ -1,3 +1,10 @@
+"""
+This script automates the double click Action Chain.
+
+@author: Raed Eleyan.
+@date: 04/03/2025.
+@contact: raedeleyan1@gmail.com
+"""
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
